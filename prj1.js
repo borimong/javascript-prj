0aeb00789e5d41136751d168a1b927c29320ea97
borimong <kimhyunsu11@naver.com>
2022-01-24 16:19:23 +0900

    final commit

--- prj1.js
+++ prj1.js
@@ -1,4 +1,11 @@
-console.log("I love you");
+let arr = ["I love you","I hate you","I don't have any idea with you"];
+
+console.log(arr[Math.floor(Math.random()*3)]);
+
+
+
+/*console.log("I love you");
 console.log("I hate you");
 console.log("I don't have any idea with you");
+console.log(arr[0]);*/
 //I love you, I hate you, I don't have any idea with you
