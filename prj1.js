0a852feb1e1c9262be3ef0781d351ae4f6e5eef4
borimong <kimhyunsu11@naver.com>
2022-01-24 16:03:03 +0900

    new change

--- prj1.js
+++ prj1.js
@@ -1,1 +1,4 @@
-console.log("Happy New Year!");
+console.log("I love you");
+console.log("I hate you");
+console.log("I don't have any idea with you");
+//I love you, I hate you, I don't have any idea with you
